1e47e3af2c0a7489f0e89cb79241cd29931d56f4
milelym <milelymarcial@gmail.com>
2018-08-27 12:15:18 -0300

    cambios en index

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
-#! /usr/bin/env node 
+#! /usr/bin/env node
 // para que leea  el bin y me permita usar el md-link por consolas
-//'use strict'; es como un eslintr pero para ejecucion
+// 'use strict'; es como un eslintr pero para ejecucion
 // sudo npm install -g
 // console.log(process.argv);para capturar el argumento que pasan en la terminal al lado de md.liks
 
-//var calculator = require('./lib/md-links');
-const colors = require('colors');
+// const colors = require('colors');
 
-var mdlinks = require('./lib/md-links');
+const mdlinks = require('./lib/md-links');
 
-const [,, ...args] = process.argv
-mdlinks.parametroRuta(args[0])
-//mdlinks.comprueba_extension(args[0])
-//console.log(mdlinks.leerArch(args);
-
-//console.log(`Add ${a} + ${b} ${calculator.add(args[0])}`.yellow);
-
-//const colors = require('colors');
+const [,, ...args] = process.argv;
+mdlinks.parametroRuta(args[0]);
 
 
 
-/*var a = 3;
-var b = 5;
+/* mdLinks(rutaAbsot)
+  .then(links => {
+    // => [{ href, text, file }]
+  })
+  .catch(console.error);
 
-console.log(`Minus ${a} + ${b} = ${calculator.minus(a,b)}`.red);
-console.log(`Multiply ${a} + ${b} = ${calculator.multiply(a,b)}`.blue);
-console.log(`Divide ${a} + ${b} = ${calculator.divide(a,b)}`.grey);
-console.log('hola'.zebra)
-console.log(`Hola ${args}`.bgCyan)
-*/
+mdLinks(rutaAbsot, { validate: true })
+  .then(links => {
+    // => [{ href, text, file, status, ok }]
+  })
+  .catch(console.error);
 // isabsolut
-//cwd indica la carpeta donde esta parada en la terminal y entrega la ruta absoluta
-
-// buffer , readfile
+// cwd indica la carpeta donde esta parada en la terminal y entrega la ruta absoluta
+// buffer , readfile*/
