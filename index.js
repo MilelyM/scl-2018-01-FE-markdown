bf1b8451dd7ad7f5ace30cd135aee874e0c68f23
milelym <milelymarcial@gmail.com>
2018-08-31 20:37:58 -0300

    exportando modulo

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 #! /usr/bin/env node
 // para que leea  el bin y me permita usar el md-link por consolas
-// 'use strict'; es como un eslintr pero para ejecucion
+'use strict'; // es como un eslintr pero para ejecucion
 // sudo npm install -g
 // console.log(process.argv);para capturar el argumento que pasan en la terminal al lado de md.liks
 
 const mdlinks = require('./lib/md-links').mdlinks;
+if (require.main === module) {
+  const options = {};
 
-
-// el primero y el segundo se ignora esta vacio y el tercero se guarda 
-const [,, ...args] = process.argv;
-
-mdlinks(args[0]).then((links)=>{
-// console.log(links)
-}).catch(err =>{
+  // el primero y el segundo se ignora esta vacio y el tercero se guarda 
+  const [,, ...args] = process.argv;
+  // 
+  if (args.includes('--validate')) options.validate = true;
+  mdlinks(args[0], options).then((links)=>{
+  // console.log(links)
+    links.forEach(element => {
+      if (options.validate) {
+        console.log(`${(element.file.cyan)} : ${(element.line)} : ${element.href} : ${element.text} : ${element.status} :${element.statusText.yellow}`);
+      } else {
+        console.log(`${element.file} : ${element.line} : ${element.href} : ${element.text} `);
+      }
+    });
+  }).catch(err =>{
   // console.log(err)
-})
+  });
+}
+module.exports = mdlinks;
 // const rutaAbsoluta = mdlinks.parametroRuta(args[0]);
 // const comprobExte = mdlinks.compruebaExtension(rutaAbsoluta); */
 // //const leerArch = mdlinks.leerArch(comprobExte);
