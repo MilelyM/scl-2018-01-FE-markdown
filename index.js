b82ff30c731750f2d3dc89fa9ac4ede41e07aad4
milelym <milelymarcial@gmail.com>
2018-09-04 11:10:24 -0300

    modificaciones index

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 #! /usr/bin/env node
-// para que leea  el bin y me permita usar el md-link por consolas
+// para que leea  el bin y me permita usar el md-link por consola
 // 'use strict'; // es como un eslintr pero para ejecucion
 // sudo npm install -g
 // console.log(process.argv);para capturar el argumento que pasan en la terminal al lado de md.liks
@@ -25,26 +25,5 @@ if (require.main === module) {
   });
 }
 module.exports = mdlinks;
-// const rutaAbsoluta = mdlinks.parametroRuta(args[0]);
-// const comprobExte = mdlinks.compruebaExtension(rutaAbsoluta); */
-// //const leerArch = mdlinks.leerArch(comprobExte);
-// const validat = args[1];
-// console.log(validat);
-// console.log(comprobExte);
-// console.log(leerArch);
 
-// console.log(rutaAbsoluta)
-/* mdLinks(rutaAbsot)
-  .then(links => {
-    // => [{ href, text, file }]
-  })
-  .catch(console.error);
-
-mdLinks(rutaAbsot, { validate: true })
-  .then(links => {
-    // => [{ href, text, file, status, ok }]
-  })
-  .catch(console.error);
-// isabsolut
-// processcwd indica la carpeta donde esta parada en la terminal y entrega la ruta absoluta
-// buffer , readfile*/
+// Nota: processcwd indica la carpeta donde esta parada en la terminal y entrega la ruta absoluta
