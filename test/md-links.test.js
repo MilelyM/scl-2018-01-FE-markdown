ea00cc501a2f85a725a00c5212421b33f88d3d4e
milelym <milelymarcial@gmail.com>
2018-09-05 09:25:20 -0300

    otro test

--- test/md-links.test.js
+++ test/md-links.test.js
@@ -7,3 +7,7 @@ const mdlinks = require('../lib/md-links');
 test('deberia verificar el archivo .md', () => {
   expect(mdlinks.compruebaExtension('/home/laboratoria/Escritorio/proyectos/scl-2018-01-FE-markdown/README.md')).toBe(true);
 });
+
+test('the data is peanut butter', () => {
+  expect(mdlinks.parametroRuta('README.md')).toBe('/home/laboratoria/Escritorio/proyectos/scl-2018-01-FE-markdown/README.md');
+});
